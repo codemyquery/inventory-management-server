4de708401ac08cb6eda9ebc76033257f1f227038
Ashutosh <ashutoshsingh5192344@gmail.com>
2023-09-12 21:38:53 +0530

    Delete DBConfig file

--- dbconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("HOST", 'localhost');
-    define("USER_NAME", 'InventoryManager');
-    define("DATABASE_NAME", 'inventory_management');
-    define("PASSWORD", 'InventoryManager@123');
-    define("API_HOME_PAGE", 'http://beytech.in/admin/api/');
-?>
