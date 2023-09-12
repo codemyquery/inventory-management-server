d29e5bcc64c07ad9188c55be6dc8b7b5bf9660ed
Ashutosh <ashutoshsingh5192344@gmail.com>
2023-09-12 21:06:29 +0530

    Updated the databse configuration

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
     define("HOST", 'localhost');
     define("USER_NAME", 'root');
-    define("PASSWORD", '');
     define("DATABASE_NAME", 'inventory_management');
+    define("PASSWORD", '');
     define("API_HOME_PAGE", 'http://localhost/inventory-management-server/');
 ?>
