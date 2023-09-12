365267387467cf9b1e890bb0f1fda969705a2789
Ashutosh <ashutoshsingh5192344@gmail.com>
2023-09-12 21:20:30 +0530

    Added database credentials

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
     define("HOST", 'localhost');
     define("USER_NAME", 'root');
-    define("DATABASE_NAME", 'inventory_management');
-    define("PASSWORD", '');
+    define("DATABASE_NAME", 'InventoryManager');
+    define("PASSWORD", 'InventoryManager@123');
     define("API_HOME_PAGE", 'http://localhost/inventory-management-server/');
 ?>
