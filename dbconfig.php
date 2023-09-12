5720ae81010f30c13fb6111dacaccbee55913a88
Ashutosh <ashutoshsingh5192344@gmail.com>
2023-09-12 21:22:23 +0530

    Added url

--- dbconfig.php
+++ dbconfig.php
@@ -3,5 +3,5 @@
     define("USER_NAME", 'root');
     define("DATABASE_NAME", 'InventoryManager');
     define("PASSWORD", 'InventoryManager@123');
-    define("API_HOME_PAGE", 'http://localhost/inventory-management-server/');
+    define("API_HOME_PAGE", 'http://beytech.in/admin/api/');
 ?>
