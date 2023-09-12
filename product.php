a822e3f13f8c1a8dc418a092e8e1d1ad487c1485
Ashutosh <ashutoshsingh5192344@gmail.com>
2023-09-12 23:39:25 +0530

    Fixed the issue of not showing product

--- product.php
+++ product.php
@@ -74,8 +74,6 @@ class Product
     function get_product_list()
     {
         $this->helper->query = "SELECT product_name id, product_name, hsn_sac, per_piece_price, quantity, created_by, date_updated FROM products "
-            . $this->helper->getFilterQuery(['productName', 'hsnSac', 'dateUpdated'])
-            . $this->helper->getSortingQuery('products',['product_name', 'dateUpdated'])
             . $this->helper->getPaginationQuery();
         $total_rows = $this->helper->query_result();
         $this->helper->query = "SELECT product_name FROM products " . $this->helper->getFilterQuery(['productName', 'hsnSac', 'dateUpdated']);
